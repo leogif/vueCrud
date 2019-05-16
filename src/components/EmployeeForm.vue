3d29572faa4530e07afe6ff2b7212e38a91141a3
leogif <leogif@gmail.com>
2019-05-16 16:05:32 -0300

    init commit

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Nome do Cliente</label>
+      <input v-model="employee.name" type="text" />
+      
+      <label>Email do Cliente</label>
+      <input v-model="employee.email" type="text" />
+      
+      <button>Adicionar Cliente</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-form',
+    data() {
+      return {
+        employee: {
+          name: '',
+          email: '',
+        },
+      }
+    },
+    methods: {
+    	handleSubmit(){
+    		this.$emit('add:employee', this.employee)
+    	},
+    },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
